fc197ff37593480603bfd17be009334f33761a4e
vishal1357v <xlr8.jpeg@gmail.com>
2026-07-14 13:33:52 +0530

    perf: remove Inter font stylesheet completely to minimize stylesheets to exactly 2

--- app/root.tsx
+++ app/root.tsx
@@ -21,14 +21,6 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
-          media="print"
-          onLoad={(e) => {
-            e.currentTarget.media = "all";
-          }}
-        />
         <Meta />
         <Links />
       </head>
