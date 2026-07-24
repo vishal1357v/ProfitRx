2ad38a5d015985fd6b0ad210dd564ac83b7b0a38
vishal1357v <xlr8.jpeg@gmail.com>
2026-07-24 22:35:36 +0530

    fix(db): use @prisma/adapter-pg for serverless PostgreSQL connectivity

--- app/db.server.ts
+++ app/db.server.ts
@@ -6,14 +6,30 @@ if (typeof BigInt !== "undefined" && !(BigInt.prototype as any).toJSON) {
 }
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
 declare global {
   // eslint-disable-next-line no-var
   var prismaGlobal: PrismaClient;
 }
 
+function createPrismaClient(): PrismaClient {
+  const connectionString = process.env.DATABASE_URL;
+  if (connectionString) {
+    try {
+      const pool = new pg.Pool({ connectionString });
+      const adapter = new PrismaPg(pool);
+      return new PrismaClient({ adapter });
+    } catch (err) {
+      console.error("[db.server.ts] Error initializing PrismaPg adapter, falling back to standard PrismaClient:", err);
+    }
+  }
+  return new PrismaClient();
+}
+
 if (!global.prismaGlobal) {
-  global.prismaGlobal = new PrismaClient();
+  global.prismaGlobal = createPrismaClient();
 }
 
 const prisma = global.prismaGlobal;
