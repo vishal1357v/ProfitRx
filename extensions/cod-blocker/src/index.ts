3557523f3f7c078a33e3238f717ed264a6c2516a
vishal1357v <xlr8.jpeg@gmail.com>
2026-09-10 18:24:49 +0530

    chore(cod-blocker): add strict typescript types to index.ts entrypoint

--- extensions/cod-blocker/src/index.ts
+++ extensions/cod-blocker/src/index.ts
@@ -1,6 +1,10 @@
+import type {
+  CartPaymentMethodsTransformRunInput,
+  CartPaymentMethodsTransformRunResult,
+} from "../generated/api";
 import { cartPaymentMethodsTransformRun, run as originalRun } from "./run";
 
-export function run(input: any) {
+export function run(input: CartPaymentMethodsTransformRunInput): CartPaymentMethodsTransformRunResult {
   return originalRun(input);
 }
 
